a9b10d533809045e6c5b23d1fb4a6130c2ede23a
BrunoMagno <bmagnoserver@gmail.com>
2024-03-29 16:55:59 -0300

    Changes by paulo

--- Ludus/src/Entidade/Produto.java
+++ Ludus/src/Entidade/Produto.java
@@ -1,4 +1,68 @@
 package Entidade;
 
 public class Produto {
+    private String nome;
+    private String genero;
+    private int anoLancamento;
+    private String plataforma;
+    private int numeroJogadores;
+    private boolean online;
+
+    public Produto(String nome, String genero, int anoLancamento, String plataforma, int numeroJogadores, boolean online) {
+        this.nome = nome;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+        this.plataforma = plataforma;
+        this.numeroJogadores = numeroJogadores;
+        this.online = online;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public int getNumeroJogadores() {
+        return numeroJogadores;
+    }
+
+    public void setNumeroJogadores(int numeroJogadores) {
+        this.numeroJogadores = numeroJogadores;
+    }
+
+    public boolean getOnline() {
+        return online;
+    }
+
+    public void setOnline(boolean online) {
+        this.online = online;
+    }
+
 }
