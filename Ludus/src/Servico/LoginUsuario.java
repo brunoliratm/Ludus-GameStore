d543b04fb5f0f03b5d36ca74ef87d98b946b5130
BrunoMagno <bmagnoserver@gmail.com>
2024-03-31 21:16:13 -0300

    update v0.1.6

--- Ludus/src/Servico/LoginUsuario.java
+++ Ludus/src/Servico/LoginUsuario.java
@@ -7,6 +7,7 @@ import Tratamento.*;
 
 public class LoginUsuario {
     static Scanner input = new Scanner(System.in);
+
     public static void loginUsuario() throws InterruptedException {
         Limpeza.limpar();
         try {
@@ -24,9 +25,45 @@ public class LoginUsuario {
                     loginUsuario();
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             Tratamentos.valorInvalido();
             loginUsuario();
-            }
         }
     }
+
+    public static void cadastrarUsuario() throws InterruptedException {
+        Limpeza.limpar();
+        try {
+            System.out.println("Digite seu CPF (apenas o número, sem pontuação): ");
+            String cpf = input.nextLine();
+            cpf = cpf.replaceAll("[^0-9]", "");
+            if (cpf.length() != 11) {
+                Tratamentos.cpfInvalido();
+                cadastrarUsuario();
+            } else {
+                System.out.println("Digite seu nome: ");
+                String nome = input.nextLine();
+                System.out.println("Digite sua senha: ");
+                String senha = input.nextLine();
+                System.out.println("Digite seu email: ");
+                String email = input.nextLine();
+                System.out.println("Digite seu telefone: ");
+                String telefone = input.nextLine();
+                System.out.println("Digite seu endereço: ");
+                String endereco = input.nextLine();
+                System.out.println("Digite sua idade: ");
+                String idade = input.nextLine();
+
+                Usuario usuario = new Usuario(cpf, nome, endereco, telefone, email, idade, senha);
+                Thread.sleep(1000);
+                RepositorioUsuario.addUsuario(usuario);
+                System.out.println("Usuário cadastrado com sucesso!");
+            }
+        }catch (Exception e) {
+            input.nextLine();
+            Tratamentos.valorInvalido();
+            cadastrarUsuario();
+        }
+
+    }
+}
--- Ludus/src/Tratamento/Tratamentos.java
+++ Ludus/src/Tratamento/Tratamentos.java
@@ -2,17 +2,22 @@ package Tratamento;
 
 public class Tratamentos extends Exception{
     public static void contaInvalida() throws InterruptedException {
-        System.out.println("Login ou senha incorretos!");
+        System.out.println("Login ou senha incorretos! tente novamente.");
         Thread.sleep(2000);
     }
 
     public static void opcaoInvalida() throws InterruptedException {
-        System.out.println("opção inválida!");
+        System.out.println("opção escolhida inválida! insira uma opção válida.");
         Thread.sleep(2000);
     }
 
     public static void valorInvalido() throws InterruptedException {
-        System.out.println("Valor inválido!");
+        System.out.println("Valor inserido inválido! insira apenaas o que é pedido.");
+        Thread.sleep(2000);
+    }
+
+    public static void cpfInvalido() throws InterruptedException {
+        System.out.println("CPF inválido! insira um CPF válido, se lembre de inserir apenas números.");
         Thread.sleep(2000);
     }
 }
